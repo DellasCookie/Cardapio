142f39d08e89f26d96102072dcdf7ff2a011527c
DellasCookie <111683942+DellasCookie@users.noreply.github.com>
2022-08-21 22:30:47 -0300

    primeiro commit
    adicionando os arquivos no github

---
+++ dist/js/app.js
@@ -0,0 +1,31 @@
+import { Cookie } from "./models/Cookies.js";
+import { numeroDeCookies } from "./controllers/numeroDeCookies.js";
+const tradicional = new Cookie('TRADICIONAL', 10);
+const chocolate = new Cookie('CHOCOLATE', 10);
+const doceAmor = new Cookie('DOCE AMOR', 12);
+const amorDeVo = new Cookie('AMOR DE VÓ', 12);
+const doseDeAmor = new Cookie('DOSE DE AMOR', 12);
+const lista = [tradicional, chocolate, doceAmor, amorDeVo, doseDeAmor];
+const cesta = [];
+const form = document.querySelectorAll('[data-form]');
+form.forEach((element) => {
+    element.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const alvo = element.attributes[0].nodeValue;
+        const quantidadeCookie = numeroDeCookies(`[data-quantidade="${alvo}"]`);
+        const item = encontraInformacoes(`[data-titulo="${alvo}"]`);
+        const precoCookie = Number(quantidadeCookie) * Number(item === null || item === void 0 ? void 0 : item.preco);
+        if (precoCookie === NaN) {
+            console.log('quantidade de cookies inválida');
+        }
+        else {
+            console.log(`O preço dos cookies ${item === null || item === void 0 ? void 0 : item.nome} é ${precoCookie}`);
+        }
+    });
+});
+function encontraInformacoes(seletor) {
+    const inptItem = document.querySelector(seletor);
+    const item = inptItem.textContent;
+    const encontra = lista.find(cookie => cookie.nome == item);
+    return encontra;
+}
---
+++ dist/js/models/Cookies.js
@@ -0,0 +1,6 @@
+export class Cookie {
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
